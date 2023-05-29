7597333df802465735ab942d6c6a68a18abb62f5
blloop <42186044+blloop@users.noreply.github.com>
2023-05-29 00:35:11 -0700

    add link parsing, copying

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+// Convert input link to output link
+// Updates output text and status text
 function convert() {
   let input = document.getElementById("input").value;
-  let doc_id = input.split("file/d/")[1].split("/view?")[0];
-  let output = "https://drive.google.com/uc?id=" + doc_id;
-  document.getElementById("output").value = output;
+  let id = (input.split("file/d/")[1] ? 
+    (input.split("file/d/")[1].split("/view?")[0] ? 
+      input.split("file/d/")[1].split("/view?")[0] : 
+      null) : 
+    null
+  );
+  let output = id ? "https://drive.google.com/uc?id=" + id : '';
+  document.getElementById("output").value = output;  
+  document.getElementById("status").innerText = (id ? 'Successfully converted' : 
+    (input ? 'Invalid input!' : 'Please paste your link above'
+    )
+  );
+}
+
+function copy() {
+  let output = document.getElementById("output").value;
+  navigator.clipboard.writeText(output);
 }
