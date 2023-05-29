12cecd715d1ec3cc05620515d1efff477bf8f1fd
blloop <42186044+blloop@users.noreply.github.com>
2023-05-29 01:05:46 -0700

    add basic styling

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ function convert() {
     null
   );
   let output = id ? "https://drive.google.com/uc?id=" + id : '';
-  document.getElementById("output").value = output;  
+  document.getElementById("output").value = output; 
   document.getElementById("status").innerText = (id ? 'Successfully converted' : 
-    (input ? 'Invalid input!' : 'Please paste your link above'
-    )
+    (input ? 'Invalid link!' : 'Please paste your link above')
+  );
+  document.getElementById("status").style.color = (id ? 'green' : 
+    (input ? 'red' : 'black')
   );
 }
 
+// Copies converted link to user clipboard
 function copy() {
   let output = document.getElementById("output").value;
   navigator.clipboard.writeText(output);
